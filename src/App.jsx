345e5bed15ac38ceb933d73d026fd768ac209d7b
Dierbek111 <dierbekrahmatullaev00@gmail.com>
2024-12-27 23:05:59 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+import Hero from './components/Hero/Hero'
+import Blog from './components/Blog/Blog'
+
+const App = () => {
+  return (
+    <>
+<Hero/>
+<Blog/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,20 @@
+import s from './Blog.module.scss'
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <>
+    <section className={s.blog}>
+        <div className="container">
+            <div className={s.grid}>
+
+
+
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Blog
